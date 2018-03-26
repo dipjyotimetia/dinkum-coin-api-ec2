6cbe07ea4477dec372fb562ea2494f981269d5fd
Stuart Parton <stuartparton@gmail.com>
2018-03-26 21:45:52 +1100

    update settings

--- build/Settings/Wgtpoc.cs
+++ build/Settings/Wgtpoc.cs
@@ -6,21 +6,17 @@
         {
             HostedZoneName = "poc.cbenv.com.au";
 
-            //AlbSecurityGroupIds = "sg-15439b72";
-            AlbSecurityGroupIds = "sg-0794177e";
-            //Ec2SecurityGroupIds = "sg-15439b72,sg-71b20a08,sg-86ab13ff";
-            Ec2SecurityGroupIds = "sg-0794177e";
+            AlbSecurityGroupIds = "sg-15439b72";
+            Ec2SecurityGroupIds = "sg-15439b72,sg-0b9b1872";
             Application.KeyPairName = "wgtpoc";
 
             InstanceType = "t2.nano";
             DesiredInstanceCount = 1;
             MinInstanceCount = 1;
-            MaxInstanceCount = 2;
-            //SubnetIds = "subnet-0a90f56e, subnet-cb46cabd, subnet-e2b41abb";
-            SubnetIds = "subnet-80d7c0d9,subnet-5bd6503c";
+            MaxInstanceCount = 1;
+            SubnetIds = "subnet-d4a4d5b0,subnet-07ac3771,subnet-e256ebbb";
             VpcCidr = "10.126.0.0/16";
-            //VpcId = "vpc-17c41d73";
-            VpcId = "vpc-2b778b4c";
+            VpcId = "vpc-17c41d73";
             LogLambdaArn = "arn:aws:lambda:ap-southeast-2:250658028269:function:CloudwatchLogsSumoTransferFunction-poc";
         }
     }
